9413d5f25423ab53cdf03f633f6c763ea7a3ea59
fred wordie <fred@mildlyupset.com>
2025-04-07 20:10:30 +0200

    added count and center text

--- outputprint.ts
+++ outputprint.ts
@@ -8,6 +8,14 @@ import moment from "npm:moment";
 
 const chrLimit = 80;
 let string = "";
+const orgList:{
+	name:string,
+	num:number
+}[] = []
+const nameList:{
+	name:string,
+	num:number
+}[] = []
 
 //Takes the JSON file filled with all the data and turns it into an array of objects
 const specSheetInfoArray = JSON.parse(
@@ -28,7 +36,6 @@ const specSheetInfoArray = JSON.parse(
 // I thınk ıts all good now....
 
 function makeLine(left: string, right: string) {
-
 	const centerLength = chrLimit - left.length - right.length;
 
 	let center = "";
@@ -44,33 +51,43 @@ function makeLine(left: string, right: string) {
 	return line + "\n";
 }
 
+function centerText(string: string) {
+	const centerPoint = (chrLimit - string.length) / 2;
+	let center = "";
+	for (let i = 0; i < centerPoint; i++) {
+		center += " ";
+	}
+	center += string;
+	return center;
+}
+
 function breakItDown(string: string) {
 	let returnString = "";
 
 	let count = 0;
 
-  //break down string into words on spaces
+	//break down string into words on spaces
 	let words = string.split(" ");
-  
-  //loop over each word
+
+	//loop over each word
 	words.forEach((word, i) => {
-    // check if it will go over line count
+		// check if it will go over line count
 		if (word.length + count > chrLimit) {
-      // break line
+			// break line
 			returnString += "\n";
-      // add  word
+			// add  word
 			returnString += word;
-      // reset count
-      count = 0
+			// reset count
+			count = 0;
 		} else {
-      // add word
+			// add word
 			returnString += word;
 		}
 
-    // add word length to count
-    count += word.length;
+		// add word length to count
+		count += word.length;
 
-    // add punctation if not lst or first words
+		// add punctation if not lst or first words
 		if (
 			i != 0 &&
 			i != 1 &&
@@ -85,10 +102,24 @@ function breakItDown(string: string) {
 		}
 	});
 
-  // return string
+	// return string
 	return returnString;
 }
 
+const addToList = (array: any[], item: string) => {
+	let index = array.findIndex((currentItem) => currentItem.item === item);
+	if (index == -1) {
+		array.push({
+			item: item,
+			num: 1,
+		});
+		return 1
+	} else {
+		array[index].num++;
+		return array[index].num
+	}
+	
+};
 
 //loop through specSheetInfoArray to add it all together in a string
 for (let i = 0; i < specSheetInfoArray.length; i++) {
@@ -99,15 +130,19 @@ for (let i = 0; i < specSheetInfoArray.length; i++) {
 	const formatedDate = moment(item.date).format("DD/MM/YYYY");
 	const docName = item.thisDocName || "Name Unknown";
 
-	string += makeLine(docName.trim(), "");
+	// string += makeLine(docName.trim(), "");
 
 	//TODO: turn the date thibgies to a dte thıngy
 
-	const date = "Date: " + formatedDate;
-	const type = "Type: " + item.type;
+	// const date = "Date: " + formatedDate;
 
-	string += makeLine(date.trim(), type.trim());
-	let tempString = "PROPERTIES DEFINED: ";
+	string += centerText(formatedDate);
+	string += "\n";
+
+	const type = "" + item.type;
+
+	string += makeLine("" + docName.trim(), type.trim());
+	let tempString = "Properties Defined: ";
 
 	if (item.properties && item.properties.length > 0) {
 		for (let i = 0; i < item.properties.length; i++) {
@@ -125,12 +160,13 @@ for (let i = 0; i < specSheetInfoArray.length; i++) {
 	if (item.authors) {
 		for (let i = 0; i < item.authors.length; i++) {
 			const name = item.authors[i].name;
-			let org = item.authors[i].org || "Org Unknown";
+			let org = "[Num:" + addToList(orgList, item.authors[i].org) +'] ';
+			org += item.authors[i].org || "Org Unknown";
 			if (org == "Invited Expert") {
 				org = org + " - Unknown Funding";
 			}
-
-			let editor = "Editor: " + name;
+			
+			let editor = "Editor: " + name + " [Num:" + addToList(nameList, name) +']';
 			//FIXME: should this be outside of this loop?
 			string += makeLine(editor.trim(), org.trim());
 		}
@@ -139,3 +175,6 @@ for (let i = 0; i < specSheetInfoArray.length; i++) {
 }
 
 await Deno.writeTextFile("output.txt", string);
+
+//TODO: Bug over line, title and type
+//TODO: add in bold
