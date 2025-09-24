7d12b7595f55777395c306e04e935296a8bdd7b5
fred wordie <fred@mildlyupset.com>
2025-09-24 16:37:55 +0200

    new stuff

---
+++ aWhodunitCodeBook.ts
@@ -0,0 +1,17 @@
+// A CSSWHODUNIT
+// Imre Ismen & Fred Wordie
+
+// Project description
+// The web is not neutral, it is not anarchic, it is standardised. It is a tapestry built on the specifications of the World Wide Web Consortium, their member organisations and their team of rotating editors. Although open in principle, the W3C is opaque in practice. Troves of information lay hidden in a labyrinth of publicly available repositories, emails, list, and web pages. Through the process of mass and unwieldy CSS specifications data scraping, this project tries to illuminate the key players, their contributions, and hence their influence in shaping our collective web.
+
+// Some Code we used in this project
+// 1. README.md
+// 2. getSpecs.ts
+// 3. getSpecInfo.ts
+// 4. getDocName.ts
+// 5. getProps.ts
+// 6. outputprint.ts
+// 7. essay.ts
+
+// Full code at
+// https://github.com/FWORDIE/cssWhoDunIt
